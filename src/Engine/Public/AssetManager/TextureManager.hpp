a11b750a3dd15044337d3fa32760c4db2a4f6089
Ch10ew <ch10ewthrowaway@gmail.com>
2022-10-27 18:28:46 +0800

    chore: Comments to TextureManager & StateMachine

--- src/Engine/Public/AssetManager/TextureManager.hpp
+++ src/Engine/Public/AssetManager/TextureManager.hpp
@@ -9,10 +9,27 @@
 
 namespace he
 {
+    /**
+     * Texture collection for the game. Textures are persistent throughout the lifetime of the program.
+     *
+     * IDs are local to the texture collection and are not shared with other collections.
+     */
     class TextureManager : public AssetManagerBase<sf::Texture>
     {
     public:
+        /**
+         * Loads a texture from `filepath`, with id of `id`.
+         *
+         * @param id Unique identifier for the texture.
+         * @param filepath Filepath to texture.
+         */
         void Load(const std::string& id, const std::string& filepath);
+
+        /**
+         * Unloads a texture with id of `id`.
+         *
+         * @param id Unique identifier for the texture.
+         */
         void Unload(const std::string& id);
         sf::Texture& Get(const std::string& id);
         const sf::Texture& Get(const std::string& id) const;
--- src/Engine/Public/Core/StateMachine.hpp
+++ src/Engine/Public/Core/StateMachine.hpp
@@ -12,42 +12,104 @@
 
 namespace he
 {
+    /**
+     * The means of scene management for the game.
+     *
+     * When starting the game, ensure there is at least one state in the state machine before calling `Game::Run()`.
+     *
+     * Implemented as a state stack, where the state at the top of the stack is the active state.
+     */
     class StateMachine
     {
     private:
         typedef std::unique_ptr<GameState> GameStateUniquePtr;
 
     public:
+        /**
+         * Construct a new State Machine object
+         */
         StateMachine();
+
+        /**
+         * Destroy the State Machine object
+         */
         ~StateMachine();
 
+    public:
+        /**
+         * Disabled Copy Constructor
+         */
         StateMachine(const StateMachine&) = delete;
+
+        /**
+         * Disabled assignment operator
+         */
         StateMachine& operator=(const StateMachine&) = delete;
 
     public:
+        /**
+         * Adds a state to the stack.
+         *
+         * Common use case is to replace the top state with a new state. In the case where a pause menu is required, a pause state could be added without replacing and popped when unpausing.
+         *
+         * @param newState Game State to be added, must be a child class of `he::GameState`.
+         * @param isReplacing Whether the active state should be replaced with the supplied new state.
+         */
         void AddState(GameStateUniquePtr newState, bool isReplacing = true);
+
+        /**
+         * Removes the most recent (top) state.
+         */
         void RemoveState();
+
+        /**
+         * Gets a pointer to the active state.
+         *
+         * @return GameStateUniquePtr& Pointer to the active state.
+         */
         GameStateUniquePtr& GetActiveState();
 
     public:
         /**
-         * @brief Called every frame before any other actions to process the pending state changes. See Game::Run().
-         *
+         * Called every frame before any other actions to process the pending state changes. See Game::Run().
          */
         void ProcessStateChanges();
 
     private:
+        /**
+         * State stack.
+         */
         std::stack<GameStateUniquePtr> _states;
+
+        /**
+         * State to be added/replaced.
+         */
         GameStateUniquePtr _newState;
 
+    private:
+        /**
+         * Flag for removing the active state on the next state change.
+         */
         bool _isRemoving = false;
+
+        /**
+         * Flag for adding the active state on the next state change.
+         */
         bool _isAdding = false;
+
+        /**
+         * Flag for replacing the active state on the next state change.
+         */
         bool _isReplacing = false;
 
 #pragma region Debug Assert
 
     private:
 #define MODULE_STATE_MACHINE_LEVEL 1
+
+        /**
+         * Setup for debug assert.
+         */
         struct ModuleStateMachine : debug_assert::default_handler,
                                     debug_assert::set_level<MODULE_STATE_MACHINE_LEVEL>
         {
